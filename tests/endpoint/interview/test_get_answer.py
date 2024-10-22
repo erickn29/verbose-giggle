364c266382fdc144a3db49080f46a850795daf1a
ya_novikov <arhdomain@gmail.com>
2024-10-22 16:58:19 +0300

    stash

--- tests/endpoint/interview/test_get_answer.py
+++ tests/endpoint/interview/test_get_answer.py
@@ -1,3 +1,5 @@
+import uuid
+
 import pytest
 
 from apps.v1.interview.repository import ChatRepository, QuestionRepository
@@ -82,3 +84,83 @@ async def test_get_answer_201(session, client, questions, auth_headers, mocker):
             raise AssertionError("No evaluation found")
         assert "Оценка: 5/10" in evaluation[0].text
         assert "Оценка ответа на вопрос" in evaluation[0].text
+
+
+async def test_get_answer_403(session, client, questions, auth_headers, mocker):
+    mock_response = mocker.patch(
+        "apps.v1.interview.utils.request.get_evaluation_request"
+    )
+    response_text = """
+    Оценка: 5/10\n
+    Оценка ответа на вопрос
+    """
+    mock_response.return_value = {"choices": [{"message": {"content": response_text}}]}
+    response = await client.post(
+        f"/api/v1/interview/a/{str(questions['chat'].id)}/",
+        headers=auth_headers["auth_headers3"],
+        json={
+            "question_id": str(questions["question"].id),
+            "text": "answer text",
+        },
+    )
+    assert response.status_code == 403
+
+
+async def test_get_answer_401(session, client, questions, auth_headers, mocker):
+    mock_response = mocker.patch(
+        "apps.v1.interview.utils.request.get_evaluation_request"
+    )
+    response_text = """
+    Оценка: 5/10\n
+    Оценка ответа на вопрос
+    """
+    mock_response.return_value = {"choices": [{"message": {"content": response_text}}]}
+    response = await client.post(
+        f"/api/v1/interview/a/{str(questions['chat'].id)}/",
+        # headers=auth_headers["auth_headers3"],
+        json={
+            "question_id": str(questions["question"].id),
+            "text": "answer text",
+        },
+    )
+    assert response.status_code == 401
+
+
+async def test_get_answer_404(session, client, questions, auth_headers, mocker):
+    mock_response = mocker.patch(
+        "apps.v1.interview.utils.request.get_evaluation_request"
+    )
+    response_text = """
+    Оценка: 5/10\n
+    Оценка ответа на вопрос
+    """
+    mock_response.return_value = {"choices": [{"message": {"content": response_text}}]}
+    response = await client.post(
+        f"/api/v1/interview/a/{str(uuid.uuid4())}/",
+        headers=auth_headers["auth_headers"],
+        json={
+            "question_id": str(questions["question"].id),
+            "text": "answer text",
+        },
+    )
+    assert response.status_code == 404
+
+
+async def test_get_answer_422(session, client, questions, auth_headers, mocker):
+    mock_response = mocker.patch(
+        "apps.v1.interview.utils.request.get_evaluation_request"
+    )
+    response_text = """
+    Оценка: 5/10\n
+    Оценка ответа на вопрос
+    """
+    mock_response.return_value = {"choices": [{"message": {"content": response_text}}]}
+    response = await client.post(
+        f"/api/v1/interview/a/{str(questions['chat'].id)}/",
+        headers=auth_headers["auth_headers"],
+        json={
+            "question_id": str(questions["question"].id),
+            # "text": "answer text",
+        },
+    )
+    assert response.status_code == 422
